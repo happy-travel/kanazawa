3f09444fbabf04b7dbe4a738998690adf79cf56b
Andrey Kotov <tralalablblbl@gmail.com>
2019-10-23 14:51:31 +0700

    NIJO-101 Add Edo-api updater project, that calls edo-api throught serviceWorker token

---
+++ HappyTravel.Edo.ProcessDeadlinePayments/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HappyTravel.Edo.ProcessDeadlinePayments
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args)
+                .Build()
+                .RunAsync();
+        }
+
+        public static IHostBuilder  CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseKestrel()
+                        .UseStartup<Startup>()
+                        .UseSentry();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var environment = hostingContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging
+                        .AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+
+                    var env = hostingContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                        logging.AddConsole();
+                    else
+                        logging.AddEventSourceLogger();
+                });
+    }
+}

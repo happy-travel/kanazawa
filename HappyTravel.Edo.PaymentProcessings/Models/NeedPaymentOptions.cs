f470f8ceffb937fce8e09ee4772b6c0fd0a6b699
Andrey Kotov <tralalablblbl@gmail.com>
2019-12-11 15:31:19 +0300

    NIJO-149 Notify client that payment is needed

---
+++ HappyTravel.Edo.PaymentProcessings/Models/NeedPaymentOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyTravel.Edo.PaymentProcessings.Models
+{
+    public class NeedPaymentOptions
+    {
+        public string GetUrl { get; set; }
+        public string ProcessUrl { get; set; }
+        public int ChunkSize { get; set; }
+    }
+}

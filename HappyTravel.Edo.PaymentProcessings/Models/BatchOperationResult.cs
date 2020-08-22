a8cf735c819e51aacb9b541ba66e419711fdc541
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-08-22 20:13:24 +0500

    Added critical logs notification on errors

---
+++ HappyTravel.Edo.PaymentProcessings/Models/BatchOperationResult.cs
@@ -0,0 +1,22 @@
+﻿namespace HappyTravel.Edo.PaymentProcessings.Models
+{
+    public readonly struct BatchOperationResult
+    {
+        public BatchOperationResult(string message, bool hasErrors)
+        {
+            Message = message;
+            HasErrors = hasErrors;
+        }
+
+
+        /// <summary>
+        ///     Process result message
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Set to true if there were errors in processing
+        /// </summary>
+        public bool HasErrors { get; }
+    }
+}
